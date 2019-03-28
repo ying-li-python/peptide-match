586a1b91030371b2e068914acf360d88f6146c02
ying-li <yinghiu.li18@gmail.com>
2019-03-28 10:00:42 -0700

    added scripts

---
+++ main.py
@@ -0,0 +1,61 @@
+"""
+Piecing together peptide data from mass-spectrometry proteomics is time-consuming and daunting. 
+
+Luckily, this script can help find the sequence location of a target protein and locate a PTM site. 
+Every peptide entered and analyzed will be recorded to a new csv file so you will have a copy of 
+your peptide analysis. 
+
+BEFORE STARTING 
+You will need to add a reference FASTA file in peptides.py for Python to match the peptide. 
+
+
+EXPECTED RESULTS 
+The program will generate a new csv file containing for every peptide sequence entered, as well 
+as the peptide location (start and end), and the PTM site. 
+
+Good luck!
+
+"""
+
+# import the functions to this file and csv module 
+from peptides import peptideSearch
+import csv 
+
+# create variable for while loop 
+ans = "y"
+
+# create variable to store list of peptides entered by user 
+peptide_list = []
+
+
+# create a new csv file and write header rows 
+with open('peptideSearch_output.csv', 'w') as csvfile: 
+    writer = csv.writer(csvfile)
+    writer.writerow(["Peptide Sequence", "Start", "End", "PTM Site"])
+
+# while loop for user to enter peptide sequence and phosphorylation site 
+while ans == "y": 
+
+    # count the number of peptides entered 
+    peptide_count = len(peptide_list)
+
+    # set conditional that once one peptide has been entered, it will track the number of peptides entered 
+    if len(peptide_list) > 0: 
+        print(f"The number of peptides you have analyzed: {peptide_count}")
+
+    # asks user for peptide sequence 
+    peptide = input("Please enter a peptide sequence to analyze: ").upper()
+
+    # add peptide sequence to peptide list 
+    peptide_list.append(peptide)
+    
+    # asks phosphorylation position. if you do not have one, please enter 0.  
+    phospho_site = int(input("Please enter the phosphorylation position on the sequence (if none, enter 0): "))
+
+    # call the function to perform the peptide analysis 
+    peptideSearch(peptide, phospho_site)
+        
+    # asks if you want to continue adding more peptide sequences     
+    ans = input("Do you want to add another peptide sequence? (y/n) ")
+    
+
---
+++ peptides.py
@@ -0,0 +1,110 @@
+"""
+This is the core part of the Python program. I created nested functions where the 
+first function reads the FASTA file as a reference sequence wnd the second is the 
+peptide match and records each output on a new line in the csv file. 
+
+REQUIREMENTS
+The entire protein sequence from FASTA must be written in a single line 
+(no breaks in between). Please visit here for guidance: 
+https://github.com/ying-li-python/fasta-fix 
+
+
+"""
+def peptideSearch(peptide, phospho_site):
+
+    # the first function (no arguments needed)
+    def fastaFile(): 
+
+        # create variable to store reference sequence 
+        ref_seq = ''
+
+        # open FASTA file, please change accordingly 
+        fastaFile = open("period.fasta", 'r')
+
+        # create for loop 
+        for line in fastaFile: 
+
+            # ignore first line in FASTA 
+            if line.startswith(">"):
+
+                # this does nothing, allows script to keep going 
+                pass
+            
+            # next line is your reference sequence, so will be stored as ref_seq 
+            else: 
+                ref_seq = line 
+
+        # the function will return the ref_seq 
+        return ref_seq
+
+    # assign output variable to store ref_seq
+    output = fastaFile()
+
+    # this is the main function to perform the peptide match
+    def peptideMatch(peptide, phospho_site): 
+        
+        # import dependencies 
+        import re
+        import csv
+        import os 
+
+        # create variables to store lists 
+        peptide_list = []
+        start_list = []
+        end_list = []
+        p_site_list = []
+
+        # use regex module to match the peptide sequece to reference sequence 
+        match = re.search(peptide, output)
+
+        # for every peptide entered, add to peptide list 
+        peptide_list.append(peptide)
+        
+        # set phosphorylation position to 0 
+        phospho_position = 0
+        
+        # create conditional if peptide matches
+        if match: 
+            # store start and end position of the reference sequence 
+            start_position = match.start()
+            start_list.append(start_position)
+
+            end_position = match.end()
+            end_list.append(end_position)
+
+            # calculate the phosphorylation site 
+            if int(phospho_site) > 0: 
+                phospho_position = start_position + phospho_site
+                p_site_list.append(phospho_position)
+
+            # if 0 is entered, return "None"
+            else: 
+                phospho_position = "None" 
+                p_site_list.append(phospho_position)
+
+            # print results in terminal
+            print("-----------------") 
+            print(f"Peptide matched and added to analysis!")
+            print("-----------------")
+
+            # compile all the lists as a tuple 
+            peptide_search_results = zip(peptide_list, start_list, end_list, p_site_list)
+
+
+            # add peptide sequence and results to current csv file 
+            with open('peptideSearch_output.csv', 'a') as csvfile: 
+
+                # initiate csvwriter 
+                peptide_writer = csv.writer(csvfile)
+
+                # write the results to each row for every peptide entered
+                peptide_writer.writerows(peptide_search_results)
+            
+        # print an error message if peptide sequence not matched  
+        else: 
+            print("-----------------") 
+            print("Peptide not found in reference FASTA sequence. Please try again.")
+            print("-----------------")
+
+    # return the results
+    return peptideMatch(peptide, phospho_site)
